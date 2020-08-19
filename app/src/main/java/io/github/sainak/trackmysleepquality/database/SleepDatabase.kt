ad340c6c572354618e503a37486a782225ba6bc7
sainAk <aakashsingh1407@gmail.com>
2020-08-19 10:12:20 +0530

    added database

---
+++ app/src/main/java/io/github/sainak/trackmysleepquality/database/SleepDatabase.kt
@@ -0,0 +1,58 @@
+package io.github.sainak.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * A database that stores SleepNight information.
+ * And a global method to get access to the database.
+ */
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    /**
+     * Connects the database to the DAO.
+     */
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    /**
+     * Define a companion object, this allows us to add functions on the SleepDatabase class.
+     */
+    companion object {
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        /**
+         * Helper function to get the database.
+         *
+         * @param context The application context Singleton, used to get access to the filesystem.
+         */
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+
+                // Copy the current value of INSTANCE to a local variable so Kotlin can smart cast.
+                // Smart cast is only available to local variables.
+                var instance = INSTANCE
+
+                // If instance is `null` make a new database instance.
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        // Wipes and rebuilds instead of migrating if no Migration object.
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    // Assign INSTANCE to the newly created database.
+                    INSTANCE = instance
+                }
+
+                // Return instance; smart cast to be non-null.
+                return instance
+            }
+        }
+    }
+}
