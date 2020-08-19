6142542e17fc8b0022c9b1a9709a4b89cefbdf35
sainAk <aakashsingh1407@gmail.com>
2020-08-19 12:38:11 +0530

    added SleepTrackerFragment

---
+++ app/src/main/java/io/github/sainak/trackmysleepquality/Util.kt
@@ -0,0 +1,38 @@
+package io.github.sainak.trackmysleepquality
+
+import android.annotation.SuppressLint
+import android.content.res.Resources
+import java.text.SimpleDateFormat
+
+
+/**
+ * Returns a string representing the numeric quality rating.
+ */
+fun convertNumericQualityToString(quality: Int, resources: Resources): String {
+    var qualityString = resources.getString(R.string.three_ok)
+    when (quality) {
+        -1 -> qualityString = "--"
+        0 -> qualityString = resources.getString(R.string.zero_very_bad)
+        1 -> qualityString = resources.getString(R.string.one_poor)
+        2 -> qualityString = resources.getString(R.string.two_soso)
+        4 -> qualityString = resources.getString(R.string.four_pretty_good)
+        5 -> qualityString = resources.getString(R.string.five_excellent)
+    }
+    return qualityString
+}
+
+
+/**
+ * Take the Long milliseconds returned by the system and stored in Room,
+ * and convert it to a nicely formatted string for display.
+ *
+ * EEEE - Display the long letter version of the weekday
+ * MMM - Display the letter abbreviation of the nmotny
+ * dd-yyyy - day in month and full year numerically
+ * HH:mm - Hours and minutes in 24hr format
+ */
+@SuppressLint("SimpleDateFormat")
+fun convertLongToDateString(systemTime: Long): String {
+    return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
+        .format(systemTime).toString()
+}
---
+++ app/src/main/java/io/github/sainak/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -0,0 +1,85 @@
+package io.github.sainak.trackmysleepquality.sleeptracker
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import io.github.sainak.trackmysleepquality.database.SleepDatabaseDao
+import io.github.sainak.trackmysleepquality.database.SleepNight
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+/**
+ * ViewModel for SleepTrackerFragment.
+ */
+class SleepTrackerViewModel(
+    val database: SleepDatabaseDao,
+    application: Application
+) : AndroidViewModel(application) {
+
+    private var tonight = MutableLiveData<SleepNight?>()
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        viewModelScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+
+        return withContext(Dispatchers.IO) {
+
+            var night = database.getTonight()
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+    }
+
+    fun onStartTracking() {
+        viewModelScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insert(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(night)
+        }
+    }
+
+    fun onStopTracking() {
+        viewModelScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+    }
+
+    fun onClear() {
+        viewModelScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+}
