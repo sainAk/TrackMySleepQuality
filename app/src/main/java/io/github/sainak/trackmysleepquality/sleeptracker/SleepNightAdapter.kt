2b6c78f57d1826b6cd4689d2ea67f149d784cd9b
sainAk <aakashsingh1407@gmail.com>
2020-08-20 12:11:31 +0530

    rename header.xml to list_item_header.xml

--- app/src/main/java/io/github/sainak/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/io/github/sainak/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -63,7 +63,7 @@ class SleepNightAdapter(private val clickListener: SleepNightListener) :
         companion object {
             fun from(parent: ViewGroup): TextViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.header, parent, false)
+                val view = layoutInflater.inflate(R.layout.list_item_header, parent, false)
                 return TextViewHolder(view)
             }
         }
